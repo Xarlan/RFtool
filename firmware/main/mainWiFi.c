4bf529e3775ff664069431317d7f53cd05032143
Xarlan <egor21@gmail.com>
2018-12-02 23:51:59 +0300

    add Queue, gitignore

--- firmware/main/mainWiFi.c
+++ firmware/main/mainWiFi.c
@@ -1,6 +1,7 @@
 // FreeRTOS component
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include "freertos/queue.h"
 
 // Wi-Fi component
 #include "esp_wifi.h"
@@ -14,6 +15,9 @@
 
 // Error library
 #include "esp_err.h"
+
+// Custom h-files
+#include "wifi.h"
 
 
 
@@ -60,12 +64,15 @@ void vUartTask(void *pvParameters)
 								.flow_ctrl = UART_HW_FLOWCTRL_DISABLE
 							 };
 
-  ESP_ERROR_CHECK(uart_param_config(UART_NUM_0, &uart_cfg));
+	ESP_ERROR_CHECK(uart_param_config(UART_NUM_0, &uart_cfg));
 
 	ESP_ERROR_CHECK(uart_set_pin(UART_NUM_0, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE));
 
 	ESP_ERROR_CHECK(uart_driver_install(UART_NUM_0, 1024, 0, 0, NULL, 0));
 
+	printf("UART Ok\n");
+
+	raw_802_11_t raw_wifi_pkt;
 
 	while( 1 )
 	{
@@ -120,7 +127,7 @@ void captured_wifi_packet(void *buff, wifi_promiscuous_pkt_type_t type)
 *******************************************************************************/
 void app_main()
 {
-	qRawWifiPkt = xQueueCreate(10, 2500);
+//	qRawWifiPkt = xQueueCreate(10, 2500);
 	xTaskCreate(vUartTask, "UartTask", 1024, NULL, 1, NULL);
 
 	wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
@@ -134,17 +141,25 @@ void app_main()
 	nvs_flash_init();
 	tcpip_adapter_init();
 
-	ESP_ERROR_CHECK(esp_event_loop_init(event_handler, NULL));
+	qRawWifiPkt = xQueueCreate(5, sizeof(raw_802_11_t));
+	if (qRawWifiPkt != NULL)
+	{
+		ESP_ERROR_CHECK(esp_event_loop_init(event_handler, NULL));
 
-	esp_wifi_init(&cfg);
-	esp_wifi_set_country(&wifi_country);
-	esp_wifi_set_storage(WIFI_STORAGE_RAM);
-	esp_wifi_set_mode(WIFI_MODE_NULL);
+		esp_wifi_init(&cfg);
+		esp_wifi_set_country(&wifi_country);
+		esp_wifi_set_storage(WIFI_STORAGE_RAM);
+		esp_wifi_set_mode(WIFI_MODE_NULL);
 
-	esp_wifi_start();
-	esp_wifi_set_channel(1, WIFI_SECOND_CHAN_NONE);
-	esp_wifi_set_promiscuous(true);
-	esp_wifi_set_promiscuous_rx_cb(&captured_wifi_packet);
+		esp_wifi_start();
+		esp_wifi_set_channel(1, WIFI_SECOND_CHAN_NONE);
+		esp_wifi_set_promiscuous(true);
+		esp_wifi_set_promiscuous_rx_cb(&captured_wifi_packet);
+	}
+	else
+	{
+		printf("Can't run task\n");
+	}
 
 }
 
--- firmware/main/wifi.h
+++ firmware/main/wifi.h
@@ -11,10 +11,13 @@
 #define WIFI_H_
 
 
+
+
 typedef struct {
 					wifi_promiscuous_pkt_type_t type;
-					wifi_promiscuous_pkt_t 		*rawPkt_802_11;
-				} esp32_raw_802_11_t;
-
+					wifi_promiscuous_pkt_t 		rawPkt_802_11[2358];		// The maximum 802.11 MTU is 2304 bytes
+				} raw_802_11_t;												// WEP : 2304 + 34 + 8 = 2346 bytes
+																			// WPA (TKIP) : 2304 + 34 + 20 = 2358 bytes
+																			// WPA2 (CCMP) : 2304 + 34 + 16 = 2354 bytes
 
 #endif /* WIFI_H_ */
